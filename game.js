a81ba5810fcbb9c3fb6c696290a52bd994183d2e
Fabricio Catae <fabricio.catae@microsoft.com>
2015-09-06 20:49:20 -0300

    prepared the files

--- game.js
+++ game.js
@@ -1,33 +1,21 @@
 /* global canvas */
 /// <reference path="babylon.2.1.d.ts" />
 function createSceneObjects(scene) {
+    // player
     var sphere = BABYLON.Mesh.CreateSphere('sphere', 32, 2, scene);
     var sph_mat = new BABYLON.StandardMaterial('sph-mat', scene);
     sph_mat.diffuseColor = new BABYLON.Color3(1, .5, .5);
     sphere.material = sph_mat;
-    var player = sphere;
-    player.update = player_update;
-    player.trail_position = null;
-    var shadow_obj = new CShadow('a');
-    var shadow = shadow_obj.createMesh(scene);
-    var camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 15, new BABYLON.Vector3(0, 0, 0), scene);
-    var light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(-1, -1, -1), scene);
-    light.intensity = .7;
-    var light2 = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(1, -1, +1), scene);
-    light2.intensity = .3;
-    var ground = BABYLON.Mesh.CreateGround('ground', 20, 20, 20, scene);
-    ground.position.y = -1;
-    ground.material = new BABYLON.StandardMaterial('ground_mat', scene);
-    ground.material.wireframe = true;
-    InputManager.init(camera, scene, player);
-    //scene.player = player;
-    scene.update = player_update;
-    return scene;
+    var player = {
+        mesh: sphere,
+        update: player_update,
+        trail_position: null
+    };
     function player_update() {
         var target_position = player.trail_position;
         if (target_position) {
-            var dx = target_position.x - player.position.x;
-            var dz = target_position.z - player.position.z;
+            var dx = target_position.x - player.mesh.position.x;
+            var dz = target_position.z - player.mesh.position.z;
             var direction = new BABYLON.Vector3(dx, 0, dz);
             if (direction.lengthSquared() > 1) {
                 direction = direction.normalize();
@@ -35,7 +23,27 @@ function createSceneObjects(scene) {
             else {
             }
             var velocity = .1;
-            player.translate(direction, velocity);
+            player.mesh.translate(direction, velocity);
         }
     }
+    // shadow
+    var shadow_obj = new CShadow('a');
+    var shadow = shadow_obj.createMesh(scene);
+    // camera, lights		
+    var camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 15, new BABYLON.Vector3(0, 0, 0), scene);
+    var light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(-1, -1, -1), scene);
+    light.intensity = .7;
+    var light2 = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(1, -1, +1), scene);
+    light2.intensity = .3;
+    // ground
+    var ground = BABYLON.Mesh.CreateGround('ground', 20, 20, 20, scene);
+    ground.position.y = -1;
+    ground.material = new BABYLON.StandardMaterial('ground_mat', scene);
+    ground.material.wireframe = true;
+    // input manager start
+    InputManager.init(camera, scene, player);
+    scene.update = function () {
+        player.update();
+    };
+    return scene;
 }
--- game.ts
+++ game.ts
@@ -3,55 +3,63 @@
 
 function createSceneObjects(scene) {
 	
+	// player
 	var sphere = BABYLON.Mesh.CreateSphere('sphere', 32, 2, scene);
 	var sph_mat = new BABYLON.StandardMaterial('sph-mat', scene);
 	sph_mat.diffuseColor = new BABYLON.Color3(1, .5, .5);
 	sphere.material = sph_mat;
-	var player = sphere;
-	player.update = player_update;
-	player.trail_position = null;
 	
+	var player = { 
+		mesh: sphere, 
+		update: player_update,
+		trail_position: null
+		};	
+		
+	function player_update() {
+		var target_position = player.trail_position ;
+
+		if(target_position) {		
+			var dx = target_position.x - player.mesh.position.x;
+			var dz = target_position.z - player.mesh.position.z;
+			
+			var direction = new BABYLON.Vector3(dx, 0, dz);
+			
+			if( direction.lengthSquared() > 1) {
+				direction = direction.normalize();
+			} else {
+				// chegou!	
+			}
+			
+			var velocity = .1;
+			
+			player.mesh.translate( direction, velocity );
+		}	
+	}	
+	
+	// shadow
 	var shadow_obj = new CShadow('a');
 	var shadow = shadow_obj.createMesh(scene);
-		
+
+	// camera, lights		
 	var camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 15, new BABYLON.Vector3(0,0,0), scene);
 	var light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(-1,-1,-1), scene); light.intensity = .7;
 	var light2 = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(1,-1, +1), scene); light2.intensity = .3;	
 
+	// ground
 	var ground = BABYLON.Mesh.CreateGround('ground', 20, 20, 20, scene);
 	ground.position.y = -1;
 	
 	ground.material = new BABYLON.StandardMaterial('ground_mat', scene);
 	ground.material.wireframe = true;
 	
+	// input manager start
 	InputManager.init(camera, scene, player);
 
-	//scene.player = player;
-	scene.update = player_update;
+	scene.update = function() {
+		player.update();
+	}; 
 	
-	return scene;
-	
-	function player_update() {
-		var target_position = player.trail_position ;
-
-	if(target_position) {		
-		var dx = target_position.x - player.position.x;
-		var dz = target_position.z - player.position.z;
-		
-		var direction = new BABYLON.Vector3(dx, 0, dz);
-		
-		if( direction.lengthSquared() > 1) {
-			direction = direction.normalize();
-		} else {
-			// chegou!	
-		}
-		
-		var velocity = .1;
-		
-		player.translate( direction, velocity );
-	}
-
-}
+	return scene;	
 }
 
 
