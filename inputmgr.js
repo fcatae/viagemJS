948fdb183367f7445677bfb4bb79332f71511b3e
Fabricio Catae <fabricio.catae@microsoft.com>
2015-09-06 21:34:39 -0300

    Shadow Class implemented

--- inputmgr.js
+++ inputmgr.js
@@ -1,22 +1,8 @@
 /// <reference path="babylon.2.1.d.ts" />
-var CShadow = (function () {
-    function CShadow(name) {
-        this._name = name;
-    }
-    CShadow.prototype.createMesh = function (scene) {
-        var shadow = BABYLON.Mesh.CreateSphere('sphere', 32, 1.5, scene);
-        var shadow_mat = new BABYLON.StandardMaterial('sph-mat', scene);
-        shadow_mat.diffuseColor = new BABYLON.Color3(1, .5, .5);
-        shadow_mat.alpha = .8;
-        shadow.material = shadow_mat;
-        return shadow;
-    };
-    return CShadow;
-})();
 var InputManager = (function () {
     function InputManager() {
     }
-    InputManager.init = function (camera, scene, player) {
+    InputManager.init = function (camera, scene) {
         // input manager
         window.addEventListener('keyup', function (evt) {
             switch (evt.keyCode) {
@@ -31,22 +17,11 @@ var InputManager = (function () {
         });
         window.addEventListener('click', function (evt) {
             var pickResult = scene.pick(scene.pointerX, scene.pointerY);
-            if (pickResult && pickResult.pickedPoint) {
+            if (pickResult && pickResult.pickedMesh && pickResult.pickedMesh.onclick) {
                 var point = pickResult.pickedPoint;
-                createTarget(point);
+                pickResult.pickedMesh.onclick(point);
             }
         });
-        // f
-        function createTarget(point) {
-            var newObject = BABYLON.Mesh.CreateTorus('torus', .5, .2, 10, scene);
-            newObject.position = point;
-            var newobj_mat = new BABYLON.StandardMaterial('gold_mat', scene);
-            newobj_mat.diffuseColor = new BABYLON.Color3(1, 1, .1);
-            newobj_mat.emissiveColor = new BABYLON.Color3(.4, .4, .2);
-            newObject.material = newobj_mat;
-            //target_position = point;
-            player.trail_position = point;
-        }
     };
     return InputManager;
 })();
--- inputmgr.ts
+++ inputmgr.ts
@@ -1,25 +1,7 @@
 /// <reference path="babylon.2.1.d.ts" />
-
-class CShadow {
-	_name: string;
-	constructor(name: string) {
-		this._name = name;
-	}
-	
-	createMesh(scene) : BABYLON.Mesh {		
-		var shadow = BABYLON.Mesh.CreateSphere('sphere', 32, 1.5, scene);
-		var shadow_mat = new BABYLON.StandardMaterial('sph-mat', scene);
-		shadow_mat.diffuseColor = new BABYLON.Color3(1, .5, .5);
-		shadow_mat.alpha = .8;
-		shadow.material = shadow_mat;
-		
-		return shadow;
-	}
-}
-
 class InputManager {
 	
-	static init(camera : BABYLON.Camera, scene: BABYLON.Scene, player: any) {
+	static init(camera : BABYLON.Camera, scene: BABYLON.Scene) {
 			
 				// input manager
 		window.addEventListener('keyup', function(evt) {
@@ -39,26 +21,13 @@ class InputManager {
 		window.addEventListener('click', function(evt) {
 			var pickResult = scene.pick(scene.pointerX, scene.pointerY);
 			
-			if( pickResult && pickResult.pickedPoint ) {		
+			if( pickResult && pickResult.pickedMesh && (<any>pickResult.pickedMesh).onclick) {
 				var point = pickResult.pickedPoint;
 				
-				createTarget(point);
+				(<any>pickResult.pickedMesh).onclick(point);
 			}
+			
 		});	
-		
-		// f
-		function createTarget(point) {
-			var newObject = BABYLON.Mesh.CreateTorus('torus', .5, .2, 10, scene);
-			newObject.position = point;			
-			
-			var newobj_mat = new BABYLON.StandardMaterial('gold_mat', scene);		
-			newobj_mat.diffuseColor = new BABYLON.Color3(1,1,.1);
-			newobj_mat.emissiveColor = new BABYLON.Color3(.4,.4,.2);
-			newObject.material = newobj_mat;
-			
-			//target_position = point;
-			player.trail_position = point;
-		}
 		
 	}
 }
