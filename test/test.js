a875269cdc7077e1329e9fdd2e65fa4eb462efdf
Fabricio Catae <fabricio.catae@microsoft.com>
2015-09-07 19:10:02 -0300

    robust collision checking

--- test/test.js
+++ test/test.js
@@ -32,3 +32,103 @@ describe('paths', function() {
 	});
 });
 
+describe('eat', function() {
+	
+	var obj1 = { name: 'obj1', x: 0, y:0, size: 1};
+	var obj2 = { name: 'obj2',x: 0, y:0.5, size: 1};
+	var obj3 = { name: 'obj3',x: 1.8, y:1.8, size: 1};
+	var obj4 = { name: 'obj4',x: 4, y:0.5, size: 1};
+
+	it('check AABB', function() {
+
+		var eat = new EatManager();
+		
+		assert.isTrue(eat.checkAABB(obj1, obj1));
+		assert.isTrue(eat.checkAABB(obj2, obj1));
+		assert.isTrue(eat.checkAABB(obj3, obj1));
+		
+		// FALSE
+		assert.isFalse(eat.checkAABB(obj4, obj1));				
+	});
+	
+	it('check distance', function() {
+
+		var eat = new EatManager();
+		
+		assert.isTrue(eat.checkDistance(obj1, obj1));
+		assert.isTrue(eat.checkDistance(obj2, obj1));
+		
+		// FALSE
+		assert.isFalse(eat.checkDistance(obj3, obj1));
+		assert.isFalse(eat.checkDistance(obj4, obj1));
+				
+	});
+	
+	it('single check collision ', function() {
+		
+		var eatManager = new EatManager();
+		var count = 0;
+		
+		var objEater = { name: 'eater', x:0, y:0, size: 1, oncollision: function(f) {			
+			count++;	
+		}};
+		
+		eatManager.registerEater(objEater);
+		eatManager.register(obj1);
+		eatManager.register(obj2);
+		eatManager.register(obj3);
+		eatManager.register(obj4);
+		eatManager.check();
+		
+		assert.equal(count, 2); // obj1 + obj2
+	});
+	
+	it('multiple eaters, check collision ', function() {
+		
+		var eatManager = new EatManager();
+		var count = 0;
+		
+		var objEaters = [{ name: 'eater', x:0, y:0, size: 1, oncollision: function(f) {			
+			count++;	
+			}},
+				{ name: 'eater2', x:4, y:0, size: 1, oncollision: function(f) {			
+					console.log('eater++');
+					count++;	
+				}}
+		];
+		
+		eatManager.registerEater(objEaters[0]);
+		eatManager.registerEater(objEaters[1]);
+		eatManager.register(obj1);
+		eatManager.register(obj2);
+		eatManager.register(obj3);
+		eatManager.register(obj4);
+		eatManager.check();
+		
+		assert.equal(count, 3); // obj1 + obj2
+	});
+	
+	
+	it('only eaters, check collision ', function() {
+		
+		var eatManager = new EatManager();
+		var count1 = 0;
+		var count2 = 0;
+		
+		var objEaters = [{ name: 'eater', x:0, y:0, size: 1, oncollision: function(f) {			
+			count1++;	
+			}},
+				{ name: 'eater2', x:1, y:0, size: 1, oncollision: function(f) {			
+					count2++;	
+				}}
+		];
+		
+		eatManager.registerEater(objEaters[0]);
+		eatManager.registerEater(objEaters[1]);
+		eatManager.check();
+		
+		assert.equal(count1, 2); // obj1 + obj2 (as eaters)
+		assert.equal(count2, 2); // obj1 + obj2 (as food)
+		
+	});
+});
---
+++ ts/eat.ts
@@ -0,0 +1,79 @@
+/// <reference path="typings/babylon.2.1.d.ts" />
+
+interface ObjectPosition {
+	name?: string;
+	x: number;
+	y: number;
+	h: number;
+	size: number;
+	oncollision: (hit) => void;
+}
+
+class MeshPosition {
+	x: number;
+	y: number;
+	h: number;
+	mesh: BABYLON.Mesh;
+	
+	constructor(mesh) {
+		this.mesh = mesh;
+	}
+	
+	change(action : (mesh) => void) {
+	
+		action && action(this.mesh);
+		
+	}
+}
+
+class EatManager {
+	
+	foodList: Array<ObjectPosition> = [];
+	eaterList: Array<ObjectPosition> = [];
+	
+	registerEater(eater) {
+		this.eaterList.push(eater);		
+		this.register(eater);
+	}
+	
+	register(food) {
+		this.foodList.push(food);
+	}
+	
+	check() {
+			
+		this.eaterList.forEach(eater => {
+			this.foodList.forEach(food => {
+				
+				var fastCheck = this.checkAABB(eater, food);
+				
+				if( fastCheck ) {					
+					var exactCheck = this.checkDistance(eater, food);
+					
+					if( exactCheck ) {
+						console.log('ae');
+						if( eater !== food ) {
+							eater.oncollision && eater.oncollision(food);
+							food.oncollision && food.oncollision(eater);
+						}  
+					}
+				}					
+
+			});						
+		});
+	}
+	
+	checkAABB(obj1: ObjectPosition, obj2: ObjectPosition) {
+		var distance = obj1.size + obj2.size;
+		
+		return ((Math.abs(obj1.x - obj2.x) <= distance) && 
+			(Math.abs(obj1.y - obj2.y) <= distance)); 
+	}
+	
+	checkDistance(obj1: ObjectPosition, obj2: ObjectPosition) {
+		var distanceSquared = (obj1.size + obj2.size)*(obj1.size + obj2.size);
+		
+		return ((obj1.x - obj2.x)*(obj1.x - obj2.x) +
+			(obj1.y - obj2.y)*(obj1.y - obj2.y)) <= distanceSquared;
+	}
+}
